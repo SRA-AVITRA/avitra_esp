037eb5a4deb5dfcfbea006ba6184361f358dc0c9
shanks-d <shshnk.sd@gmail.com>
2019-10-07 15:26:15 +0530

    Added test codes for rpm

---
+++ test/rpm_using_freq/main/include/encoder.h
@@ -0,0 +1,25 @@
+#ifndef _ENCODER_H
+#define _ENCODER_H
+
+#include "init_sra.h"
+
+typedef struct{
+    char  name[10];
+    int   id;
+    int   curr_rpm;
+    volatile long int ticks_count;
+    volatile long int total_ticks;
+    gpio_num_t enc_intr;
+    gpio_num_t enc_dir;
+    esp_timer_handle_t periodic_timer;
+}encoder_t;
+
+void init_encoder(encoder_t *encoder);    //TODO: add parameter for type of edge
+
+void IRAM_ATTR enc_isr_handler(encoder_t* encoder);
+
+void IRAM_ATTR calculate_rpm(encoder_t* encoder);
+
+void setup_rpm_calculator(encoder_t* encoder);
+
+#endif
---
+++ test/rpm_using_freq/main/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+#include "init_sra.h"
+#include "pin_defs.h"
+#include "motor.h"
+
+motor_t motor_L = (motor_t) {.name = "MOTOR_L", .id = 1, .desr_rpm = 200, .Kp = 0.003,  .Kd = 0.75, .duty_cycle = 0,  \
+                        .encoder = {.name = "MOTOR_L", .id = 1, .curr_rpm = 0, .ticks_count = 0, .enc_intr = ENCODER_L_A, .enc_dir = ENCODER_L_B},  \
+                        .pwm_A = {.pwm_unit = MCPWM_UNIT_0, .pwm_timer = MCPWM_TIMER_1, .pwm_operator = MCPWM_OPR_A, .pwm_io_signals = MCPWM1A, .pwm_pin = MOTOR_L_PWM_A}, \
+                        .pwm_B = {.pwm_unit = MCPWM_UNIT_0, .pwm_timer = MCPWM_TIMER_1, .pwm_operator = MCPWM_OPR_B, .pwm_io_signals = MCPWM1B, .pwm_pin = MOTOR_L_PWM_B}  \
+                    };
+
+motor_t motor_R = (motor_t) {.name = "MOTOR_R", .id = 2, .desr_rpm = 200, .Kp = 0.003,  .Kd = 0.75, .duty_cycle = 0, \
+                        .encoder = {.name = "MOTOR_R", .id = 2, .curr_rpm = 0, .ticks_count = 0, .enc_intr = ENCODER_R_A, .enc_dir = ENCODER_R_B},  \
+                        .pwm_A = {.pwm_unit = MCPWM_UNIT_1, .pwm_timer = MCPWM_TIMER_0, .pwm_operator = MCPWM_OPR_A, .pwm_io_signals = MCPWM0A, .pwm_pin = MOTOR_R_PWM_A}, \
+                        .pwm_B = {.pwm_unit = MCPWM_UNIT_1, .pwm_timer = MCPWM_TIMER_0, .pwm_operator = MCPWM_OPR_B, .pwm_io_signals = MCPWM0B, .pwm_pin = MOTOR_R_PWM_B}  \
+                    };
+
+void initialize()
+{
+    init_motor(&motor_L);
+    init_motor(&motor_R);
+    gpio_set_direction(MOTOR_L_PWM_A, GPIO_MODE_OUTPUT); 
+    gpio_set_direction(MOTOR_L_PWM_B, GPIO_MODE_OUTPUT); 
+    gpio_set_direction(MOTOR_R_PWM_A, GPIO_MODE_OUTPUT);        
+    gpio_set_direction(MOTOR_R_PWM_B, GPIO_MODE_OUTPUT);   
+    gpio_set_level(MOTOR_L_PWM_A, 0);
+    gpio_set_level(MOTOR_L_PWM_B, 1);  
+    gpio_set_level(MOTOR_R_PWM_A, 0);
+    gpio_set_level(MOTOR_R_PWM_B, 1);   
+
+}
+
+void app_main()
+{
+    initialize();
+    while(true){
+        printf("Ticks_L = %ld\t Ticks_R = %ld\n",motor_L.encoder.total_ticks,motor_R.encoder.total_ticks);
+        vTaskDelay(1 / portTICK_RATE_MS);
+    }
+}
