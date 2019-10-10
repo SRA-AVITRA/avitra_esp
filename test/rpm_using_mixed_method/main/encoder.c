bd7f62f449cc4e91f4363d2dbd91b0f7c8803965
swapnil <swapnilmane1393@gmail.com>
2019-10-10 17:08:11 +0530

    mixed method for rpm calculation is tested and working properly

--- test/rpm_using_mixed_method/main/encoder.c
+++ test/rpm_using_mixed_method/main/encoder.c
@@ -4,7 +4,7 @@
 #include "encoder.h"
 #include "pin_defs.h"
 
-#define RPM_FACTOR 1000000*60/135
+#define RPM_FACTOR 444444.444444444     //1000000*60/135
 
 int64_t time_gap_lower_limit = 0;
 int64_t time_gap_upper_limit = 0;
@@ -16,13 +16,26 @@ int rpm = 0;
 
 void IRAM_ATTR enc_isr_handler(encoder_t* encoder)
 {
+    time_gap_lower_limit = esp_timer_get_time();
     if(gpio_get_level(encoder->enc_dir)== 0){
         encoder->ticks_count++;
     }
     else{
         encoder->ticks_count--;
     }
-    time_gap_lower_limit = esp_timer_get_time();
+}
+
+void setup_rpm_calculator(encoder_t* encoder){
+    printf("setup_rpm_calculator()\n");
+    const esp_timer_create_args_t periodic_timer_args = {
+            .callback = calculate_rpm,
+            .arg = encoder,
+            .name = encoder->name
+    };
+
+    esp_timer_create(&periodic_timer_args, &(encoder->periodic_timer));
+    esp_timer_start_periodic(encoder->periodic_timer, 44444);
+    printf("setup_rpm_calculator() ENDS\n");
 }
 
 void init_encoder(encoder_t* encoder){
@@ -37,32 +50,21 @@ void init_encoder(encoder_t* encoder){
 
 void calculate_rpm(encoder_t* encoder)
 {
+    time_gap_upper_limit = esp_timer_get_time();
     const_period_count++;
     if(encoder->ticks_count > 0)
     {
-        time_gap_upper_limit = esp_timer_get_time();
         pre_time_gap = current_time_gap;
         current_time_gap = time_gap_upper_limit - time_gap_lower_limit;
         eq_time = 44444*const_period_count + pre_time_gap - current_time_gap;
         rpm = (encoder->ticks_count * RPM_FACTOR)/eq_time;
-        
-        printf("encoder->ticks_count = %ld\t eq_time = %lld\t rpm = %d\t pre_time_gap = %lld\t current_time_gap = %lld\t const_period_count = %d\n",encoder->ticks_count, eq_time, rpm, pre_time_gap, current_time_gap, const_period_count);
-        
+        printf("encoder->ticks_count = %ld\t rpm = %d\n", encoder->ticks_count, rpm);     
         encoder->ticks_count = 0;
         const_period_count = 0;
     }
-    vTaskDelay(1 / portTICK_RATE_MS);
-}
-
-void setup_rpm_calculator(encoder_t* encoder){
-    printf("setup_rpm_calculator()\n");
-    const esp_timer_create_args_t periodic_timer_args = {
-            .callback = calculate_rpm,
-            .arg = encoder,
-            .name = encoder->name
-    };
-
-    esp_timer_create(&periodic_timer_args, &(encoder->periodic_timer));
-    esp_timer_start_periodic(encoder->periodic_timer, 44444);
-    printf("setup_rpm_calculator() ENDS\n");
+    else
+    {
+        rpm = 0;
+    }
+    vTaskDelay(10 / portTICK_RATE_MS);
 }
