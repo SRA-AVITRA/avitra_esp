f38d1f5699aa02edce7e80bd371f29eddb0dc85e
Shweta <shwetakum98@gmail.com>
2019-10-11 17:58:13 +0530

    Removed the RPM part from encoder test

--- test/encoder_test/main/encoder_test.c
+++ test/encoder_test/main/encoder_test.c
@@ -19,26 +19,25 @@ void init_encoder(encoder_t* encoder){
     init_gpio(encoder->enc_dir, GPIO_MODE_INPUT);
     init_interrupt(encoder->enc_intr);
 	gpio_isr_handler_add(encoder->enc_intr, enc_isr_handler, (void*) encoder);
-    setup_rpm_calculator(encoder);
+    setup_ticks_calculator(encoder);
     printf("init_encoder() END\n");
 }
 
-void calculate_rpm(encoder_t* encoder){
-    encoder->curr_rpm = encoder->ticks_count * 10.0001;    //(1000000*60)/(135*111111) = 4.0000400004
+void calculate_ticks(encoder_t* encoder){
     encoder->total_ticks +=  encoder->ticks_count;
     encoder->ticks_count = 0;
     vTaskDelay(1 / portTICK_RATE_MS);
 }
 
-void setup_rpm_calculator(encoder_t* encoder){
+void setup_ticks_calculator(encoder_t* encoder){
     printf("setup_rpm_calculator()\n");
     const esp_timer_create_args_t periodic_timer_args = {
-            .callback = calculate_rpm,
+            .callback = calculate_ticks,
             .arg = encoder,
             .name = encoder->name
     };
 
     esp_timer_create(&periodic_timer_args, &(encoder->periodic_timer));
     esp_timer_start_periodic(encoder->periodic_timer, 44444);
-    printf("setup_rpm_calculator() ENDS\n");
+    printf("setup_ticks_calculator() ENDS\n");
 }
