64895a8ebec07e5dda11f1bb77f4fc9ec4116b4a
shanks-d <shshnk.sd@gmail.com>
2019-12-16 20:38:44 +0530

    Removed build folder

--- test/BNO055_test/build/SRA/component_project_vars.mk
+++
@@ -1,8 +0,0 @@
-# Automatically generated build file. Do not edit.
-COMPONENT_INCLUDES += $(IDF_PATH)/components/SRA/include
-COMPONENT_LDFLAGS += -L$(BUILD_DIR_BASE)/SRA -lSRA
-COMPONENT_LINKER_DEPS += 
-COMPONENT_SUBMODULES += 
-COMPONENT_LIBRARIES += SRA
-COMPONENT_LDFRAGMENTS += 
-component-SRA-build: 
