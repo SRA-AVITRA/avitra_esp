38cee4d0b03e561c766655d7f1e27564d8812eb7
sm1393 <swapnilmane1393@gmail.com>
2020-02-01 16:34:54 +0530

    Edited base params for live data monitoring

---
+++ components/rosserial_esp32/include/auto_nav/base_params_msg.h
@@ -0,0 +1,230 @@
+#ifndef _ROS_auto_nav_base_params_msg_h
+#define _ROS_auto_nav_base_params_msg_h
+
+#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include "ros/msg.h"
+
+namespace auto_nav
+{
+
+  class base_params_msg : public ros::Msg
+  {
+    public:
+      typedef int32_t _ticks_L_type;
+      _ticks_L_type ticks_L;
+      typedef int32_t _ticks_R_type;
+      _ticks_R_type ticks_R;
+      typedef float _duty_cycle_L_type;
+      _duty_cycle_L_type duty_cycle_L;
+      typedef float _duty_cycle_R_type;
+      _duty_cycle_R_type duty_cycle_R;
+      typedef float _desr_rpm_L_type;
+      _desr_rpm_L_type desr_rpm_L;
+      typedef float _desr_rpm_R_type;
+      _desr_rpm_R_type desr_rpm_R;
+      typedef float _curr_rpm_L_type;
+      _curr_rpm_L_type curr_rpm_L;
+      typedef float _curr_rpm_R_type;
+      _curr_rpm_R_type curr_rpm_R;
+
+    base_params_msg():
+      ticks_L(0),
+      ticks_R(0),
+      duty_cycle_L(0),
+      duty_cycle_R(0),
+      desr_rpm_L(0),
+      desr_rpm_R(0),
+      curr_rpm_L(0),
+      curr_rpm_R(0)
+    {
+    }
+
+    virtual int serialize(unsigned char *outbuffer) const
+    {
+      int offset = 0;
+      union {
+        int32_t real;
+        uint32_t base;
+      } u_ticks_L;
+      u_ticks_L.real = this->ticks_L;
+      *(outbuffer + offset + 0) = (u_ticks_L.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_ticks_L.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_ticks_L.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_ticks_L.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->ticks_L);
+      union {
+        int32_t real;
+        uint32_t base;
+      } u_ticks_R;
+      u_ticks_R.real = this->ticks_R;
+      *(outbuffer + offset + 0) = (u_ticks_R.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_ticks_R.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_ticks_R.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_ticks_R.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->ticks_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_duty_cycle_L;
+      u_duty_cycle_L.real = this->duty_cycle_L;
+      *(outbuffer + offset + 0) = (u_duty_cycle_L.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_duty_cycle_L.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_duty_cycle_L.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_duty_cycle_L.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->duty_cycle_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_duty_cycle_R;
+      u_duty_cycle_R.real = this->duty_cycle_R;
+      *(outbuffer + offset + 0) = (u_duty_cycle_R.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_duty_cycle_R.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_duty_cycle_R.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_duty_cycle_R.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->duty_cycle_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_desr_rpm_L;
+      u_desr_rpm_L.real = this->desr_rpm_L;
+      *(outbuffer + offset + 0) = (u_desr_rpm_L.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_desr_rpm_L.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_desr_rpm_L.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_desr_rpm_L.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->desr_rpm_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_desr_rpm_R;
+      u_desr_rpm_R.real = this->desr_rpm_R;
+      *(outbuffer + offset + 0) = (u_desr_rpm_R.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_desr_rpm_R.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_desr_rpm_R.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_desr_rpm_R.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->desr_rpm_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_curr_rpm_L;
+      u_curr_rpm_L.real = this->curr_rpm_L;
+      *(outbuffer + offset + 0) = (u_curr_rpm_L.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_curr_rpm_L.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_curr_rpm_L.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_curr_rpm_L.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->curr_rpm_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_curr_rpm_R;
+      u_curr_rpm_R.real = this->curr_rpm_R;
+      *(outbuffer + offset + 0) = (u_curr_rpm_R.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_curr_rpm_R.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_curr_rpm_R.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_curr_rpm_R.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->curr_rpm_R);
+      return offset;
+    }
+
+    virtual int deserialize(unsigned char *inbuffer)
+    {
+      int offset = 0;
+      union {
+        int32_t real;
+        uint32_t base;
+      } u_ticks_L;
+      u_ticks_L.base = 0;
+      u_ticks_L.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_ticks_L.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_ticks_L.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_ticks_L.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->ticks_L = u_ticks_L.real;
+      offset += sizeof(this->ticks_L);
+      union {
+        int32_t real;
+        uint32_t base;
+      } u_ticks_R;
+      u_ticks_R.base = 0;
+      u_ticks_R.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_ticks_R.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_ticks_R.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_ticks_R.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->ticks_R = u_ticks_R.real;
+      offset += sizeof(this->ticks_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_duty_cycle_L;
+      u_duty_cycle_L.base = 0;
+      u_duty_cycle_L.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_duty_cycle_L.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_duty_cycle_L.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_duty_cycle_L.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->duty_cycle_L = u_duty_cycle_L.real;
+      offset += sizeof(this->duty_cycle_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_duty_cycle_R;
+      u_duty_cycle_R.base = 0;
+      u_duty_cycle_R.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_duty_cycle_R.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_duty_cycle_R.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_duty_cycle_R.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->duty_cycle_R = u_duty_cycle_R.real;
+      offset += sizeof(this->duty_cycle_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_desr_rpm_L;
+      u_desr_rpm_L.base = 0;
+      u_desr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_desr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_desr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_desr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->desr_rpm_L = u_desr_rpm_L.real;
+      offset += sizeof(this->desr_rpm_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_desr_rpm_R;
+      u_desr_rpm_R.base = 0;
+      u_desr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_desr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_desr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_desr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->desr_rpm_R = u_desr_rpm_R.real;
+      offset += sizeof(this->desr_rpm_R);
+      union {
+        float real;
+        uint32_t base;
+      } u_curr_rpm_L;
+      u_curr_rpm_L.base = 0;
+      u_curr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_curr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_curr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_curr_rpm_L.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->curr_rpm_L = u_curr_rpm_L.real;
+      offset += sizeof(this->curr_rpm_L);
+      union {
+        float real;
+        uint32_t base;
+      } u_curr_rpm_R;
+      u_curr_rpm_R.base = 0;
+      u_curr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_curr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_curr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_curr_rpm_R.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->curr_rpm_R = u_curr_rpm_R.real;
+      offset += sizeof(this->curr_rpm_R);
+     return offset;
+    }
+
+    const char * getType(){ return "auto_nav/base_params_msg"; };
+    const char * getMD5(){ return "87254fcf19f3263ef70bff812025b0a0"; };
+
+  };
+
+}
+#endif
--- components/rosserial_esp32/include/auto_nav/tuning_msg.h
+++ components/rosserial_esp32/include/auto_nav/tuning_msg.h
@@ -18,11 +18,17 @@ namespace auto_nav
       _Ki_type Ki;
       typedef float _Kd_type;
       _Kd_type Kd;
+      typedef float _alpha_type;
+      _alpha_type alpha;
+      typedef float _iTerm_limit_type;
+      _iTerm_limit_type iTerm_limit;
 
     tuning_msg():
       Kp(0),
       Ki(0),
-      Kd(0)
+      Kd(0),
+      alpha(0),
+      iTerm_limit(0)
     {
     }
 
@@ -59,6 +65,26 @@ namespace auto_nav
       *(outbuffer + offset + 2) = (u_Kd.base >> (8 * 2)) & 0xFF;
       *(outbuffer + offset + 3) = (u_Kd.base >> (8 * 3)) & 0xFF;
       offset += sizeof(this->Kd);
+      union {
+        float real;
+        uint32_t base;
+      } u_alpha;
+      u_alpha.real = this->alpha;
+      *(outbuffer + offset + 0) = (u_alpha.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_alpha.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_alpha.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_alpha.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->alpha);
+      union {
+        float real;
+        uint32_t base;
+      } u_iTerm_limit;
+      u_iTerm_limit.real = this->iTerm_limit;
+      *(outbuffer + offset + 0) = (u_iTerm_limit.base >> (8 * 0)) & 0xFF;
+      *(outbuffer + offset + 1) = (u_iTerm_limit.base >> (8 * 1)) & 0xFF;
+      *(outbuffer + offset + 2) = (u_iTerm_limit.base >> (8 * 2)) & 0xFF;
+      *(outbuffer + offset + 3) = (u_iTerm_limit.base >> (8 * 3)) & 0xFF;
+      offset += sizeof(this->iTerm_limit);
       return offset;
     }
 
@@ -98,11 +124,33 @@ namespace auto_nav
       u_Kd.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
       this->Kd = u_Kd.real;
       offset += sizeof(this->Kd);
+      union {
+        float real;
+        uint32_t base;
+      } u_alpha;
+      u_alpha.base = 0;
+      u_alpha.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_alpha.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_alpha.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_alpha.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->alpha = u_alpha.real;
+      offset += sizeof(this->alpha);
+      union {
+        float real;
+        uint32_t base;
+      } u_iTerm_limit;
+      u_iTerm_limit.base = 0;
+      u_iTerm_limit.base |= ((uint32_t) (*(inbuffer + offset + 0))) << (8 * 0);
+      u_iTerm_limit.base |= ((uint32_t) (*(inbuffer + offset + 1))) << (8 * 1);
+      u_iTerm_limit.base |= ((uint32_t) (*(inbuffer + offset + 2))) << (8 * 2);
+      u_iTerm_limit.base |= ((uint32_t) (*(inbuffer + offset + 3))) << (8 * 3);
+      this->iTerm_limit = u_iTerm_limit.real;
+      offset += sizeof(this->iTerm_limit);
      return offset;
     }
 
     const char * getType(){ return "auto_nav/tuning_msg"; };
-    const char * getMD5(){ return "efa8beee6353230a5babb632ca65705b"; };
+    const char * getMD5(){ return "725010a6824889adc4983723e3d2a44b"; };
 
   };
 
